bc8552cd7dc8fd3951073937fa9c35e06a40f5af
Kien Do <kien.do@liferay.com>
2017-09-30 17:12:07 -0700

    Add express server Move index.html from dist to root

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+app.get(
+  '*',
+  (req, res) => res.sendFile(
+    '/index.html',
+    {root: __dirname}
+  )
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
