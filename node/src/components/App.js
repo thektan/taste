cb5045b0bfb56964509ea6173ea8fe78973e65aa
Kien Do <kien.do@liferay.com>
2017-10-24 13:49:57 -0700

    The most generic route should be placed at the bottom

--- node/src/components/App.js
+++ node/src/components/App.js
@@ -1,31 +1,34 @@
 import JSXComponent from 'metal-jsx';
 import Router from 'metal-router';
 
-import Navbar from './Navbar';
-import Home from '../pages/Home';
 import About from '../pages/About';
+import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Navbar from './Navbar';
 import SignUp from '../pages/SignUp';
 
 import '../css/App.scss';
 
 class App extends JSXComponent {
-	attached() {
-		Router.router().dispatch();
-	}
+  attached() {
+    Router.router().dispatch();
+  }
 
-	render() {
-		return (
-			<div id="app">
-				<Navbar />
+  render() {
+    return (
+      <div id="app">
+        <Navbar />
 
-				<Router component={Home} path="/" />
-				<Router component={About} path="/about" />
-				<Router component={Login} path="/login" />
-				<Router component={SignUp} path="/signup" />
-			</div>
-		);
-	}
+        <Router component={About} path="/about" />
+
+        <Router component={Login} path="/login" />
+
+        <Router component={SignUp} path="/signup" />
+
+        <Router component={Home} path="/" />
+      </div>
+    );
+  }
 }
 
 new App({element: '#root'});
