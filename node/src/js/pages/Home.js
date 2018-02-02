4186244b4bf86b51d43f32e02c0d85af12fa8dd8
Kevin Tan <kevin.tan@liferay.com>
2018-02-02 09:20:46 -0800

    Add example of reactstrap

--- node/src/js/pages/Home.js
+++ node/src/js/pages/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { UncontrolledAlert } from 'reactstrap';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+
+        <UncontrolledAlert color="info">
+          This is a test for reactstrap.
+        </UncontrolledAlert>
       </div>
     );
   }
