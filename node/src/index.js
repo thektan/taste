63565caaf3af209b299f3e7f1e932f3ed7e4af9d
Kevin Tan <kevin.tan@liferay.com>
2018-02-02 09:14:41 -0800

    Add reactstrap to use bootstrap v4.0

--- node/src/index.js
+++ node/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.css';
+
 import App from './js/App';
 import registerServiceWorker from './js/utils/registerServiceWorker';
 
