1b8095b3714236f686ce045e7c54ff29793be15f
Kevin Tan <kevin.tan@liferay.com>
2018-02-02 10:15:37 -0800

    GH-39 Markup for login page

--- node/src/js/App.js
+++ node/src/js/App.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  Navbar,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 import '../css/App.css';
 
 import Home from './pages/Home';
@@ -8,20 +20,49 @@ import User from './pages/User';
 // import UIKit from './js/ui-kit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <Router>
         <div>
-          <h1>Taste</h1>
+          <Navbar color="faded" light expand="md">
+            <NavbarBrand href="/">Taste</NavbarBrand>
 
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/User">About</a>
-            </li>
-          </ul>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/user">Github</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Navbar>
 
           <Route exact path="/" component={Home} />
 
